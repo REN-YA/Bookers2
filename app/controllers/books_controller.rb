2f752c94613b180a1801795183452a423d72987b
EC2 Default User <ec2-user@ip-172-31-10-8.ec2.internal>
2021-12-20 08:01:39 +0000

    [add]Bootstrap・投稿機能作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,12 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
   def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_show
   end
 
   def index
@@ -12,5 +16,11 @@ class BooksController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def book_params
+    params.requrire(:book).permit(:titel, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   resource :users
   resource :books
-  
+
 end
